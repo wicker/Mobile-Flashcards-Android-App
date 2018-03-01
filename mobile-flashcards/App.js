2a62294029f3d1c90aa007f3f1eb3f3231ef9b64
Jenner Hanni <jenner@wickerbox.net>
2018-03-01 15:26:39 -0800

    feat: Create initial data object

--- mobile-flashcards/App.js
+++ mobile-flashcards/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage } from 'react-native'
+import initialData from './utils/initialData'
 
 export default class App extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <Text>Test</Text>
+      {Object.values(initialData)
+        .map(data =>
+          <Text key={ data.title }>{ data.title }</Text>
+        )
+      }
       </View>
     );
   }
